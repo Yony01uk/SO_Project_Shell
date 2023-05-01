919f46348d4659fe83a340c95c4f0b5001f46c76
Yony01uk <131732643+Yony01uk@users.noreply.github.com>
2023-05-01 06:39:57 -0400

    Add files via upload

--- Outputs/outputsCommands.h
+++ Outputs/outputsCommands.h
@@ -5,32 +5,61 @@
 
 //EL PARAMETRO *file SIEMPRE DEBE SER LA RUTA COMPLETA DEL ARCHIVO PARA EVITAR CONFUSIONES POSTERIORES
 
-int LeftAngular(char *output,char *file)
+int LeftAngular(char *command_id,char *file)
 {
-    FILE *file_dest = fopen(file,"w");
-    fputs(output,file_dest);
-    fclose(file_dest);
-    return 0;
-}
-
-int DoubleLeftAngular(char *output,char *file)
-{
-    FILE *file_dest = fopen(file,"w");
-    if(file_dest == NULL)
-    {
-        creat(file,PERMS);
-        file_dest = fopen(file,"w");
-    }
-    fputs(output,file_dest);
-    fclose(file_dest);
-    return 0;
-}
-
-int RightAngular(char *file)
-{
-    FILE *file_src = fopen(file,"r");
-    if(file_src == NULL)
+    char *file_location = (char *)malloc(sizeof(char)*256);
+    file_location = GetFileName(command_id,DIR_OUT);
+    FILE *f_out = fopen(file,"r");
+    if(f_out == NULL)
         return -1;
-    fclose(file_src);
+    fclose(f_out);
+    f_out = fopen(file_location,"w");
+    fputs(file,f_out);
+    fclose(f_out);
+    return 0;
+}
+
+int DoubleLeftAngular(char *command_id,char *file)
+{
+    char *file_location = (char *)malloc(sizeof(char)*256);
+    file_location = GetFileName(command_id,DIR_OUT);
+    FILE *f_out = fopen(file,"r");
+    if(f_out == NULL)
+        creat(file,PERMS);
+    f_out = fopen(file_location,"w");
+    fputs(file,f_out);
+    fclose(f_out);
+    return 0;
+}
+
+int RightAngular(char *command_id,char *file)
+{
+    char *file_location = (char *)malloc(sizeof(char)*256);
+    file_location = GetFileName(command_id,DIR_INPUT);
+    FILE *f_out = fopen(file,"r");
+    if(f_out == NULL)
+        return -1;
+    fclose(f_out);
+    f_out = fopen(file_location,"w");
+    fputs(file,f_out);
+    fclose(f_out);
+    return 0;
+}
+
+int Pipe(char *command_id_1,char *command_id_2)
+{
+    char *file_location1 = (char *)malloc(sizeof(char)*256);
+    char *file_location2 = (char *)malloc(sizeof(char)*256);
+    char *data = (char *)malloc(sizeof(char)*256);
+    file_location1 = GetFileName(command_id_1,DIR_OUT);
+    file_location2 = GetFileName(command_id_2,DIR_INPUT);
+    FILE *file = fopen(file_location1,"r");
+    data = fgets(data,sizeof(data),file);
+    fclose(file);
+    if(strcmp(data,"NONE") == 0)
+        file_location1 = GetFileName(command_id_1,OUTPUT);
+    file = fopen(file_location2,"w");
+    fputs(file_location1,file);
+    fclose(file);
     return 0;
 }
--- Outputs/outputsCommandsWorker.c
+++ Outputs/outputsCommandsWorker.c
@@ -7,5 +7,5 @@ int main(int argc, char *argv[])
     if(strcmp(argv[0],"d_l_a") == 0)
         return DoubleLeftAngular(argv[1],argv[2]);
     if(strcmp(argv[0],"r_a") == 0)
-        return RightAngular(argv[1]);
+        return RightAngular(argv[1],argv[2]);
 }
